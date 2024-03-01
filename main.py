661511b4d49b5ee74b76c00478db0db4f485a828
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-03-01 10:14:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def num_of_pumpkins(matrix):
     result = []
-    for i in range(len(matrix)):
-        if i % 2 == 0:
+    for i in range(len(matrix)-1,-1,-1):
+        if i % 2 != 0:
             result.extend((matrix[i]))
         else:
             result.extend(matrix[i][::-1])
