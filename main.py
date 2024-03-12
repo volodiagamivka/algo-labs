402c4327e65a0e21256f9e5e1d69b19a3be40d94
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-03-12 19:07:45 +0200

    Update code in main.py

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-    pivot_pos = array[0]
-
-    left = []
-    right = []
-    center = []
-    for el in array:
-        if el == pivot_pos:
-            center.append(el)
-        elif el < pivot_pos:
-            left.append(el)
+def binary_search(x, left, right, array):
+    while left <= right:
+        center = (left + right) // 2
+        if array[center] == x:
+            return True
+        elif array[center] < x:
+            left = center + 1
         else:
-            right.append(el)
-    return quick_sort(left) + center + quick_sort(right)
+            right = center - 1
+    return False
 
 
 def finding_nums(array, p):
-    sorted_arr = quick_sort(array)
+    sorted_arr = sorted(array)
     for i in range(len(sorted_arr) - 2):
         j = i + 1
         k = len(sorted_arr) - 1
         while j < k:
-            if  sorted_arr[j] + sorted_arr[k] == p-sorted_arr[i]:
+            num_sum = sorted_arr[j] + sorted_arr[k]
+            if num_sum + sorted_arr[i] == p:
                 return True
-            elif  sorted_arr[j] + sorted_arr[k] < p-sorted_arr[i]:
-                j += 1
+            elif  num_sum < p:
+                if binary_search(p - num_sum, j + 1, k, sorted_arr):
+                    return True
+                else:
+                    j+=1
+                    
             else:
                 k -= 1
     return False
