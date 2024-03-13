7617e97059caa6f64fabcac2b46e744ada50e35d
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-03-13 12:27:37 +0200

    add solution to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branch_sums(root):
+    if root is None:
+        return 0
+
+    if root.left is not None:
+        if root.left.left is None and root.left.right is None:
+            return root.left.value + branch_sums(root.right)
+
+    return branch_sums(root.left) + branch_sums(root.right)
 
