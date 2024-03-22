4f353905a0e48068b952b7591182e9dd14f96a18
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-03-22 13:11:19 +0200

    Update heap_based_priority_queue.py

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -8,7 +8,7 @@ class PriorityQueue:
     def __init__(self):
         self.queue = []
 
-    def heap_sort(self, array, n, i):
+    def __heapify(self, array, n, i):
         max_index = i
         left_child = 2 * i + 1
         right_child = 2 * i + 2
@@ -21,21 +21,21 @@ class PriorityQueue:
 
         if max_index != i:
             array[i], array[max_index] = array[max_index], array[i]
-            self.heap_sort(array, n, max_index)
+            self.__heapify(array, n, max_index)
 
     def insert_to_queue(self, value, priority):
         node = Node(value, priority)
         self.queue.append(node)
         n = len(self.queue)
         for i in range(n // 2, -1, -1):
-            self.heap_sort(self.queue,n, i)
+            self.__heapify(self.queue,n, i)
 
     def delete_max_pr_el(self):
         if self.queue:
             el_for_deleting = self.queue[0]
             self.queue[0] = self.queue[-1]
             self.queue.pop()
-            self.heap_sort(self.queue, len(self.queue), 0)
+            self.__heapify(self.queue, len(self.queue), 0)
             return el_for_deleting
 
     def display_queue(self):
