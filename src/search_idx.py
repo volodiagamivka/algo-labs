caa23d885ae6c657ac3b2af49dcc28f85b611f21
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-04-22 13:59:17 +0300

    Update search_idx.py

--- src/search_idx.py
+++ src/search_idx.py
@@ -1,21 +1,23 @@
 def search_idx_rabbin_karp(needle, haystack):
     n = len(haystack)
     m = len(needle)
-    q = 10
+    base = 11
+    divisor = 21
+
     hash_of_needle = 0
     hash_of_haystack = 0
 
     for i in range(m):
-        hash_of_needle += ord(needle[i]) * (q ** (m - i - 1))
-        hash_of_haystack += ord(haystack[i]) * (q ** (m - i - 1))
+        hash_of_needle += ord(needle[i]) * (base ** (m - i - 1)) % divisor
+        hash_of_haystack += ord(haystack[i]) * (base ** (m - i - 1)) % divisor
 
-    for i in range(n - m + 1):
-        if hash_of_haystack == hash_of_needle:
+    for i in range(n - m - 1):
+        if hash_of_haystack % divisor == hash_of_needle % divisor:
             if haystack[i : i + m] == needle:
                 return i
         if i < n - m:
             hash_of_haystack = (
-                hash_of_haystack - ord(haystack[i]) * (q ** (m - 1))
-            ) * q + ord(haystack[i + m])
+                hash_of_haystack - ord(haystack[i]) * (base ** (m - 1))
+            ) * base + ord(haystack[i + m]) % divisor
 
     return "Не знайдено індексів входження"
