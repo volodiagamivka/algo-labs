84959222bcda67eaaf055c09303f80eab7824048
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-05-16 16:21:22 +0300

    Update test_search_idx.py

--- tests/test_search_idx.py
+++ tests/test_search_idx.py
@@ -1,7 +1,5 @@
-import sys
 
-sys.path.append("/Users/voloda/Desktop/algo/lab7/src")
-from search_idx import search_idx_rabbin_karp
+from src.search_idx import search_idx_rabbin_karp
 import unittest
 
 
@@ -12,12 +10,7 @@ class TestSearchIdx(unittest.TestCase):
         expected_result = "Не знайдено індексів входження"
         self.assertEqual(search_idx_rabbin_karp(needle, haystack), expected_result)
 
-    def test_one_occurence(self):
-        haystack = "crocodile"
-        needle = "dil"
-        expeected_result = 5
-        self.assertEqual(search_idx_rabbin_karp(needle, haystack), expeected_result)
-
+    
     def test_collision(self):
         haystack = "ababababababab"
         needle = "abab"
