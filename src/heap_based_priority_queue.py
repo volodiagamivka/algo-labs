ea3d4281a89bba6553b57573bb25a612be0ccb52
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-05-16 16:15:05 +0300

    Update heap_based_priority_queue.py

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -3,12 +3,12 @@ class Node:
         self.value = value
         self.priority = priority
 
-
 class PriorityQueue:
     def __init__(self):
         self.queue = []
 
-    def __heapify(self, array, n, i):
+    def __heapify_down(self, array, n, i):
+        
         max_index = i
         left_child = 2 * i + 1
         right_child = 2 * i + 2
@@ -21,24 +21,35 @@ class PriorityQueue:
 
         if max_index != i:
             array[i], array[max_index] = array[max_index], array[i]
-            self.__heapify(array, n, max_index)
+            self.__heapify_down(array, n, max_index)
+
+    def __heapify_up(self, array, i):
+        
+        parent = (i - 1) // 2
+        if i > 0 and array[i].priority > array[parent].priority:
+            array[i], array[parent] = array[parent], array[i]
+            self.__heapify_up(array, parent)
 
     def insert_to_queue(self, value, priority):
+        
         node = Node(value, priority)
         self.queue.append(node)
-        n = len(self.queue)
-        for i in range(n // 2, -1, -1):
-            self.__heapify(self.queue,n, i)
+        self.__heapify_up(self.queue, len(self.queue) - 1)
 
     def delete_max_pr_el(self):
-        if self.queue:
-            el_for_deleting = self.queue[0]
-            self.queue[0] = self.queue[-1]
-            self.queue.pop()
-            self.__heapify(self.queue, len(self.queue), 0)
-            return el_for_deleting
+        
+        if not self.queue:
+            return None
+        max_node = self.queue[0]
+        self.queue[0] = self.queue[-1]
+        self.queue.pop()
+        self.__heapify_down(self.queue, len(self.queue), 0)
+        return max_node
 
     def display_queue(self):
+        
         for node in self.queue:
-            print(f"Значення:{node.value},Пріоритет:{node.priority}")
+            print(f"Значення: {node.value}, Пріоритет: {node.priority}")
+
+
 
