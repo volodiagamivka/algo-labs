da622414ccac2897c64042cd8ad5eac5896c155c
volodiagamivka <101040262+volodiagamivka@users.noreply.github.com>
2024-05-20 13:26:43 +0300

    Update indian_johns.py

--- src/indian_johns.py
+++ src/indian_johns.py
@@ -3,33 +3,21 @@ def number_of_routes():
         width, height = map(int, input_data.readline().strip().split())
         matrix = [input_data.readline().strip() for _ in range(height)]
 
-    num_of_ways = [
-        [0] * width for i in range(height)
-    ]  # create a matrix, each element in the matrix is set to 0
+    num_of_ways = [[0] * width for _ in range(height)]
 
     for row in range(height):
         num_of_ways[row][0] = 1
 
     for col in range(1, width):
+        paths = {}
         for row in range(height):
-            # Direct move from the left
+            if matrix[row][col] not in paths:
+                paths[matrix[row][col]] = 0
+            paths[matrix[row][col]] += num_of_ways[row][col - 1]
+        for row in range(height):
+            num_of_ways[row][col] += paths[matrix[row][col]]
 
-            num_of_ways[row][col] += num_of_ways[row][col - 1]
-
-            # Check possible jumps from previous columns with the same symbol
-            for i in range(height):
-                if matrix[row][col] == matrix[i][col - 2]:
-                    num_of_ways[row][col] += num_of_ways[i][col - 2]
-            # Check possible diagonal jumps upwards
-            if row > 0 and matrix[row][col] == matrix[row - 1][col - 1]:
-                num_of_ways[row][col] += num_of_ways[row - 1][col - 1]
-
-            # Check possible diagonal jumps downwards
-            if row < height - 1 and matrix[row][col] == matrix[row + 1][col - 1]:
-                num_of_ways[row][col] += num_of_ways[row + 1][col - 1]
-
-    # return a result from the first and last rows of the last columns
-    result = num_of_ways[0][width - 1] + num_of_ways[height - 1][width - 1]
+    result = sum(num_of_ways[row][width - 1] for row in range(height))
 
     with open("ijones.out", "w") as output_data:
         output_data.write(str(result))
